d900ad0faf5cac6bc06011b59b11526ab09afb05
Bill <bill80362@gmail.com>
2025-01-10 23:13:44 +0800

    LINE帳號

---
+++ database/migrations/0002_01_01_000001_insert_permissions_table_line.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        $g = \App\Models\PermissionGroup::findOrNew(12);
+        $g->id = 12;
+        $g->name = "LINE帳號管理";
+        $g->save();
+        //
+        $p = \App\Models\Permission::findOrNew(45);
+        $p->id = 45;
+        $p->permission_group_id = $g->id;
+        $p->name = "讀取";
+        $p->save();
+        //
+        $p = \App\Models\Permission::findOrNew(46);
+        $p->id = 46;
+        $p->permission_group_id = $g->id;
+        $p->name = "新增";
+        $p->save();
+        //
+        $p = \App\Models\Permission::findOrNew(47);
+        $p->id = 47;
+        $p->permission_group_id = $g->id;
+        $p->name = "修改";
+        $p->save();
+        //
+        $p = \App\Models\Permission::findOrNew(48);
+        $p->id = 48;
+        $p->permission_group_id = $g->id;
+        $p->name = "刪除";
+        $p->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+    }
+};
