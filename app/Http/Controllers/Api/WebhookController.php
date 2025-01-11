9891cbdcaf428ccdd3de28959f13450625d10b59
Bill <bill80362@gmail.com>
2025-01-11 11:30:13 +0800

    LINE訊息接收

--- app/Http/Controllers/Api/WebhookController.php
+++ app/Http/Controllers/Api/WebhookController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Line;
+use App\Models\LineMessages;
+use Illuminate\Support\Facades\Request;
 
 class WebhookController extends Controller
 {
-    public function line($id)
+    public function line(Request $request,$id)
     {
-        
+        $line = Line::findOrFail($id);
+
+        $lineMessages = new LineMessages();
+        $lineMessages->status = "I";
+        $lineMessages->line_id = $line->id;
+        $lineMessages->log = $request->getContent();
+        $lineMessages->save();
+
+        $signature = base64_encode(hash_hmac('sha256', $request->getContent() , $line->secret, true));
+        if ($signature != $request->header('http_x_line_signature')) {
+            throw new \Exception("sign error");
+        }
+
+        $data = $request->json();
+        foreach ($data["events"]??[] as $event){
+            if ($event['message']['type'] == 'text') {
+                //記錄訊息內容
+                $lineMessages->type = "T";
+                $lineMessages->message = $event['message']['text']??"";;
+                $lineMessages->member_line_id = $event['source']['userId']??"";
+                $lineMessages->message_at = date("Y-m-d H:i:s",$event['timestamp']??"");
+                $lineMessages->save();
+            }
+        }
     }
 }
