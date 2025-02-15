023116f0b11407eb401c286ac0a371dbd164ce60
Bill <bill80362@gmail.com>
2025-02-15 12:47:34 +0800

    開發期間

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>店小二會員門市系統</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
