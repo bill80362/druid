4946850d7d5bf383678b25b45c172b0c40e8def1
Bill <bill80362@gmail.com>
2025-06-03 13:17:40 +0800

    保母

--- resources/views/babysitter/search_search.blade.php
+++ resources/views/babysitter/search_search.blade.php
@@ -89,6 +89,7 @@
 
 <!-- Modal -->
 <form>
+    <input type="hidden" name="userId" value="{{request()->get('userId')}}">
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
