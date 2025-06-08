7401194c1b3061cf3e053a1eed9c3fa21f2c04cb
Bill <bill80362@gmail.com>
2025-06-08 14:02:28 +0800

    保母

--- routes/console.php
+++ routes/console.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\Artisan;
 //    $this->comment(Inspiring::quote());
 //})->purpose('Display an inspiring quote')->hourly();
 
-Artisan::command('running-log', function () {
-    $this->comment(\App\Console\Commands\Log\RunningLog::class);
-})->purpose('排程運行中紀錄')->everyMinute();
-
-Artisan::command('load-data', function () {
-    $this->comment(\App\Console\Commands\Babysitter\BabysitterLoader::class);
-})->purpose('載入保母資訊')->everyTwoHours();
+//Artisan::command('running-log', function () {
+//    $this->comment(\App\Console\Commands\Log\RunningLog::class);
+//})->purpose('排程運行中紀錄')->everyMinute();
+//
+//Artisan::command('load-data', function () {
+//    $this->comment(\App\Console\Commands\Babysitter\BabysitterLoader::class);
+//})->purpose('載入保母資訊')->everyTwoHours();
